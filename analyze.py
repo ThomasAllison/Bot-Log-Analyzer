fa9947ed8b61c98ffdc7a831b16523ab74711c98
Thomas Allison <thomas.allison@byte.nl>
2017-10-11 12:22:49 +0200

    Use directory as argument

--- analyze.py
+++ analyze.py
@@ -3,6 +3,8 @@ from collections import Counter
 
 import sys
 
+import os
+
 log_file_dir = "/home/thomas/Documents/Dev/logs/hnaccesslogs/access.log.1"
 
 count_bots = 0
@@ -35,21 +37,45 @@ def do_something_with_line(line):
         count_bot()
 
 
-# ============ START SCRIPT =============
+def read_from_dir(directory):
+    count = 0
+    total = len(os.listdir(directory))
+    for filename in os.listdir(directory):
+        path_to_file = directory + filename
+        read_from_file(path_to_file)
 
-for arg in sys.argv[1:]:
+        count += 1
+        one_string = "Done reading with {} of {} ".format(count, total) + path_to_file
+        print(one_string)
+
+
+def read_from_file(file):
     # Open log file in 'read' mode
-    with open(arg, "r") as in_file:
+    with open(file, "r") as in_file:
+        global total_lines
         # Loop over each log line
         for string_line in in_file:
             line = ast.literal_eval(string_line)
             do_something_with_line(line)
             total_lines += 1
 
+
+
+# ============ START SCRIPT =============
+
+for arg in sys.argv[1:]:
+    if os.path.isdir(arg):
+        read_from_dir(arg)
+    elif os.path.isfile(arg):
+        read_from_file(arg)
+
+
+
+
 # ============ PRINT STATS =============
 
 print("")
-percentage = (float(42140)/float(90457)) * 100.00
+percentage = (float(count_bots)/float(total_lines)) * 100.00
 one_string = str(count_bots) + " bots\n" + str(total_lines) + " total\n" + str(percentage)+"%" + " is bot\n"
 print(one_string)
 
