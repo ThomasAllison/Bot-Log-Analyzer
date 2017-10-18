c558229dc00c97cd89a17e63d30ac759558ecd26
Thomas Allison <thomas.allison@byte.nl>
2017-10-18 15:54:37 +0200

    Improve speed

--- analyze.py
+++ analyze.py
@@ -1,10 +1,10 @@
-import ast
 from collections import Counter
-import sys
 import os
 import re
 
-# log_file_dir = "/home/thomas/Documents/Dev/logs/hnaccesslogs/access.log.1"
+import sys
+
+log_file_dir = "/home/thomas/Documents/Dev/logs/lesslogs/one"
 filters = ["bot", "magereport", "facebook", "crawler", "slurp", "tws", "spider", "scan"]
 
 count_bots = 0
@@ -42,6 +42,7 @@ def write_bot_list(file, list, title="List", minimum=0):
         if freq > minimum:
             file.write("{:<8} {:<15}\n".format(freq, useragent))
 
+
 def write_stats(file):
     file.write("\n====================  {}  =================\n\n".format("Stats"))
 
@@ -50,32 +51,32 @@ def write_stats(file):
     file.write(one_string)
 
 
-def do_something_with_line(line):
+def do_something_with_useragent_string(line):
     if is_bot(line):
         useragent_contains_bot(line)
     else:
-        legit_list.append(line["user_agent"])
+        legit_list.append(line)
 
 
 def is_bot(line):
-    useragent = line["user_agent"]
-    urls_in_useragent = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', useragent)
-    email_in_useragent = re.findall('[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+', useragent)
+    if any(n in line.lower() for n in filters):
+        return True
+    elif line is "-":
+        return True
+    elif line is "":
+        return True
 
-    if any(n in useragent.lower() for n in filters):
-        return True
-    elif useragent is "-":
-        return True
-    elif useragent is "":
-        return True
-    elif urls_in_useragent:
-        return True
+    email_in_useragent = re.findall('[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+', line)
     if email_in_useragent:
+        return True
+
+    urls_in_useragent = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', line)
+    if urls_in_useragent:
         return True
 
 
 def useragent_contains_bot(line):
-    add_bot_to_list(line["user_agent"])
+    add_bot_to_list(line)
     count_bot()
 
 
@@ -94,7 +95,7 @@ def read_from_dir(directory):
             read_from_file(path_to_file)
 
             count += 1
-            one_string = "Done reading with {} of {} ".format(count, total) + path_to_file
+            one_string = "Done reading with {} of {}".format(count, total) + path_to_file
             print(one_string)
 
 
@@ -120,10 +121,18 @@ def read_from_file(file):
         global total_lines
         # Loop over each log line
         for string_line in in_file:
-            line = ast.literal_eval(string_line)
-            do_something_with_line(line)
+            line = find_text_between_strings(string_line, '"user_agent":"', '",')
+            do_something_with_useragent_string(line)
             total_lines += 1
 
+
+def find_text_between_strings(s, first, last):
+    try:
+        start = s.index(first) + len(first)
+        end = s.index(last, start)
+        return s[start:end]
+    except ValueError:
+        return ""
 
 
 # ============ START SCRIPT =============
@@ -133,25 +142,23 @@ for arg in sys.argv[1:]:
         walk_dir(arg)
     elif os.path.isfile(arg):
         read_from_file(arg)
-        print("Done reading single file "+arg)
+        print("Done reading single file " + arg)
 
 
 # ============ PRINT STATS =============
 
 print("")
-percentage = (float(count_bots)/float(total_lines)) * 100.00
-one_string = str(count_bots) + " bots\n" + str(total_lines) + " total\n" + str(percentage)+"%" + " is bot\n"
+percentage = (float(count_bots) / float(total_lines)) * 100.00
+one_string = str(count_bots) + " bots\n" + str(total_lines) + " total\n" + str(percentage) + "%" + " is bot\n"
 print(one_string)
 
 print_bot_list(bot_list)
-# print("\n====================================================================\n")
-# print_bot_list(legit_list)
+print_bot_list(legit_list)
 
 
 # ============ WRITE STATS =============
+
 f = open("loganalysis.txt", "w+")
 write_stats(f)
 write_bot_list(f, bot_list, title="Bot List")
 write_bot_list(f, legit_list, title="Legit List", minimum=10)
-
-
